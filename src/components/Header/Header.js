cf1cbbf577b275a905e6b95a1ca3b47b6fc6a7cf
Sergey Tsybulkin <sereg1ns@yandex.ru>
2023-09-01 14:02:15 +0300

    feat: components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header";
+
+function Header() {
+  return <header className="header">
+    <div>24422442</div>
+  </header>
+}
+
+export default Header
---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Main";
+
+function Main() {
+  return <header className="main">
+    <div className="main__content">12</div>
+  </header>
+}
+
+export default Main
