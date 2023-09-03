2fea01a22b1444b3e23e1d83652e1fea71f85932
Sergey Tsybulkin <sereg1ns@yandex.ru>
2023-09-03 15:25:50 +0300

    feat: profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,36 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
-import Header from "../Header/Header";
-import Main from "../Main/Main";
-import Footer from "../Footer/Footer";
-import Register from "../Register/Register";
-import Login from "../Login/Login"
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import Profile from '../Profile/Profile';
 
 function App() {
   return (
-  <div className="page">
-  <section className="page__content">
-    <Switch>
-      <Route path="/" exact>
-        <Header />
-        <Main />
-        <Footer />
-      </Route>
-      <Route path="/signup">
-        <Header />
-        <Register />
-        <Footer />
-      </Route>
-      <Route path="/signin">
-        <Header />
-        <Login />
-        <Footer />
-      </Route>
+    <div className="page">
+      <section className="page__content">
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+            <Main />
+            <Footer />
+          </Route>
+          <Route path="/signup">
+            <Header />
+            <Register />
+            <Footer />
+          </Route>
+          <Route path="/signin">
+            <Header />
+            <Login />
+            <Footer />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
         </Switch>
       </section>
     </div>
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Profile.css";
+import Header from "../Header/Header";
+
+function Profile() {
+  return (
+    <>
+      <Header />
+      <section className="profile">
+        <h2 className="profile__title">Привет, Сергей!</h2>
+        <form id="form" className="profile__form" noValidate>
+          <label className="profile__label">
+            Имя
+            <input
+              name="name"
+              className="profile__input"
+              id="name-input"
+              type="text"
+              placeholder="Сергей"
+              minLength="2"
+              maxLength="40"
+              required
+            />
+            <span className="profile__input-error">{}</span>
+          </label>
+          <label className="profile__label">
+            E-mail
+            <input
+              name="email"
+              className="profile__input"
+              id="email-input"
+              type="email"
+              placeholder="pochta@yandex.ru"
+              required
+            />
+            <span className="profile__input-error">{}</span>
+          </label>
+          <button type="button" className="profile-button__edit hover">
+            Редактировать
+          </button>
+          <button type="button" className="profile-button__signout hover">
+            Выйти из аккаунта
+          </button>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Profile;
