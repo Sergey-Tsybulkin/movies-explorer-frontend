ab05e72010f74cbb5a51597c051ecdf44810a43c
Sergey Tsybulkin <sereg1ns@yandex.ru>
2023-09-16 13:12:09 +0300

    feat: const plus utils

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+const SHORTMOVIES_DURATION = 40;
+
+const SHOW_MORE_RES_1280_PLUS = 3;
+const SHOW_MORE_RES_768 = 2;
+const SHOW_MORE_320_480 = 1;
+
+const USER_NAME_REGEXP = '^[A-Za-zА-Яа-яЁё /s -]+$';
+
+const EMAIL_REGEXP =
+  "^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$";
+
+export {
+  SHORTMOVIES_DURATION,
+  SHOW_MORE_RES_1280_PLUS,
+  SHOW_MORE_RES_768,
+  SHOW_MORE_320_480,
+  USER_NAME_REGEXP,
+  EMAIL_REGEXP,
+};
---
+++ src/utils/utils.js
@@ -0,0 +1,27 @@
+import { SHORTMOVIES_DURATION } from './constants';
+
+export function filterShortMovies(movies) {
+  return movies.filter((movie) => movie.duration < SHORTMOVIES_DURATION);
+}
+
+export function transformDuration(duration) {
+  const hours = Math.trunc(duration / 60);
+  const minutes = duration % 60;
+  if (hours === 0) {
+    return `${minutes}м`;
+  } else {
+    return `${hours}ч ${minutes}м`;
+  }
+}
+
+export function filterMovies(movies, query) {
+  const moviesByUserQuery = movies.filter((movie) => {
+    const movieRu = String(movie.nameRU).toLowerCase().trim();
+    const movieEn = String(movie.nameEN).toLowerCase().trim();
+    const userMovie = query.toLowerCase().trim();
+    return (
+      movieRu.indexOf(userMovie) !== -1 || movieEn.indexOf(userMovie) !== -1
+    );
+  });
+  return moviesByUserQuery;
+}
